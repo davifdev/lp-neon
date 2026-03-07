bc8b3473c0fa22567c860c46680ceab1b52fed50
Davi Fernandes <davi.fer159@gmail.com>
2026-03-07 16:34:11 -0300

    chore: add initial config

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,21 @@ import uglify from "gulp-uglify";
 const sass = gulpSass(dartSass);
 
 // Compilando o sass adicionando autoprefixed e dando refresh na página.
-function compilaSass() {
+function sassCompiler() {
   return gulp
     .src("./scss/*.scss")
     .pipe(sass({ style: "compressed" }))
-    .pipe(
-      autoPrefixer({
-        overrideBrowserslist: ["last 2 versions"],
-        cascade: false,
-      }),
-    )
+    .pipe(autoPrefixer({ cascade: false }))
     .pipe(gulp.dest("./css"))
     .pipe(browserSync.stream());
 }
 
 // Tarefa do sass
-gulp.task("sass", compilaSass);
+gulp.task("sass", sassCompiler);
 
-function gulpJS() {
+function jsCompiler() {
   return gulp
-    .src("js/*.js")
+    .src("js/scripts/*.js")
     .pipe(concat("main.js"))
     .pipe(
       gulpBabel({
@@ -40,7 +35,7 @@ function gulpJS() {
     .pipe(browserSync.stream());
 }
 
-gulp.task("mainjs", gulpJS);
+gulp.task("mainjs", jsCompiler);
 
 // Função do BrowserSync
 function browser() {
@@ -56,9 +51,9 @@ gulp.task("browser-sync", browser);
 
 // Função do watch para alterações em scss e html
 function watch() {
-  gulp.watch("./scss/*.scss", compilaSass);
+  gulp.watch("./scss/*.scss", sassCompiler);
   gulp.watch("*.html").on("change", browserSync.reload);
-  gulp.watch("js/*js", gulpJS);
+  gulp.watch("./js/scripts/*.js", jsCompiler);
 }
 
 // Tarefa do watch
