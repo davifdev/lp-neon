4f05b1adff529342385f1d5aa9b3db683dbcf4cc
Davi Fernandes <davi.fer159@gmail.com>
2026-03-12 20:23:12 -0300

    feat: add responsive section account digital

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-var swiper=new Swiper(".depoyments",{slidesPerView:3,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+var swiper=new Swiper(".depoyments",{slidesPerView:3,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{368:{slidesPerView:1.5},768:{slidesPerView:2},800:{slidesPerView:1.5},991:{slidesPerView:3}}});
--- js/scripts/script.js
+++ js/scripts/script.js
@@ -5,4 +5,18 @@ const swiper = new Swiper(".depoyments", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    368: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 1.5,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
 });
