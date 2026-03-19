7dbe74d952b127744b2c06d8c3ee48a8f2b7b64f
Davi Fernandes <davi.fer159@gmail.com>
2026-03-18 21:09:07 -0300

    feat: add animations and dropdown menu

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -23,10 +23,32 @@ const swiper = new Swiper(".depoyments", {
 
 const btnMobile = document.querySelector(".btn-mobile");
 const areaMenu = document.querySelector(".area-menu");
+const btnDropdown = document.querySelector(".btn-open-dropdown");
+const dropdown = document.querySelector(".dropdown");
+const header = document.querySelector(".js-header");
 
 function handleClick() {
   areaMenu.classList.toggle("active");
 }
 
+function openDropdownMenu() {
+  dropdown.classList.toggle("active");
+}
+
 btnMobile.addEventListener("click", handleClick);
 areaMenu.addEventListener("click", handleClick);
+btnDropdown.addEventListener("click", openDropdownMenu);
+dropdown.addEventListener("mouseleave", openDropdownMenu);
+
+document.addEventListener("scroll", (e) => {
+  if (scrollY > 80) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
